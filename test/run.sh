f9a0475671c5aa74745e8f1ac8e1ddfd8dfef689
mij001 <mayurainomall@gmail.com>
2024-03-09 12:14:26 +0530

    boot vm, wait for ssh

---
+++ test/run.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -e
+cd "$(dirname "$0")/.."
+V=.vm
+[ -f $V/disk.qcow2 ] || sh test/setup.sh
+
+ACCEL=tcg; CPU=max
+[ -w /dev/kvm ] && { ACCEL=kvm; CPU=host; }
+
+qemu-system-x86_64 \
+	-M q35 -accel $ACCEL -cpu $CPU -m ${VM_RAM:-2560} -smp ${VM_CPUS:-4} \
+	-drive file=$V/disk.qcow2,if=virtio,format=qcow2 \
+	-drive file=$V/seed.iso,if=virtio,format=raw,readonly=on \
+	-netdev user,id=n0,hostfwd=tcp::2222-:22 \
+	-device virtio-net-pci,netdev=n0 \
+	-serial file:$V/console.log -monitor none -display none \
+	-pidfile $V/pid &
+
+echo "waiting for ssh..."
+for i in $(seq 1 150); do
+	if sh test/ssh.sh true 2>/dev/null; then echo "vm up"; exit 0; fi
+	sleep 2
+done
+echo "timeout; see $V/console.log"; exit 1
