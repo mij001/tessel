4612005f468dc231fa235a161e18050ae5a496e7
mij001 <mayurainomall@gmail.com>
2024-05-10 20:46:03 +0530

    output destroy left it in the list

--- src/output.cpp
+++ src/output.cpp
@@ -15,5 +15,6 @@ static void output_destroy(wl_listener *l, void *data) {
 	Output *o = wl_container_of(l, o, destroy);
 	wl_list_remove(&o->frame.link);
 	wl_list_remove(&o->destroy.link);
+	wl_list_remove(&o->link);
 	delete o;
 }
