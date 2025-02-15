a4a68ba915a5f14549ceb50ef9863a3008c9c549
mij001 <mayurainomall@gmail.com>
2025-02-16 02:37:08 +0530

    set_tiled. clients drew rounded corners

--- src/tiling.cpp
+++ src/tiling.cpp
@@ -4,4 +4,6 @@ static void place(View *v, int x, int y, int w, int h) {
 	v->geo = {x, y, w, h};
 	wlr_scene_node_set_position(&v->tree->node, x, y);
 	wlr_xdg_toplevel_set_size(v->toplevel, w, h);
+	wlr_xdg_toplevel_set_tiled(v->toplevel,
+		WLR_EDGE_TOP | WLR_EDGE_BOTTOM | WLR_EDGE_LEFT | WLR_EDGE_RIGHT);
 }
