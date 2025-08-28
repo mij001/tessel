fb00e739f9042c26d6c2471beb56bac77203acc9
mij001 <mayurainomall@gmail.com>
2025-08-28 19:42:32 +0530

    session script

--- test/sync.sh
+++ test/sync.sh
@@ -2,6 +2,6 @@
 # push the source into the test vm, build and install it there.
 set -e
 cd "$(dirname "$0")/.."
-tar cf - Makefile src test 2>/dev/null \
+tar cf - Makefile src test tessel-session tessel.desktop 2>/dev/null \
 	| sh test/ssh.sh 'rm -rf ~/tessel && mkdir -p ~/tessel && tar xf - -C ~/tessel'
 sh test/ssh.sh 'cd ~/tessel && make 2>&1 | tail -5 && sudo make install 2>&1 | tail -3'
